8a726d2c0cddeff33187530a941d710364d9c32c
Benjamin Borbe <bborbe@rocketnews.de>
2016-05-19 22:02:37 +0200

    implement reader who copy the read content

---
+++ reader_shadow_copy/reader_shadow_copy_test.go
@@ -0,0 +1,73 @@
+package reader_shadow_copy
+
+import (
+	"testing"
+
+	"bytes"
+	"io"
+	"io/ioutil"
+
+	. "github.com/bborbe/assert"
+)
+
+func TestImplementsReadCloser(t *testing.T) {
+	b := New(nil)
+	var expected *io.ReadCloser
+	if err := AssertThat(b, Implements(expected)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func newReaderCloser(content string) io.ReadCloser {
+	return NopCloser(bytes.NewBufferString(content))
+}
+
+func TestImplementsShadow(t *testing.T) {
+	content := "hello world"
+	b := New(newReaderCloser(content))
+	org, err := ioutil.ReadAll(b)
+	if err := AssertThat(err, NilValue()); err != nil {
+		t.Fatal(err)
+	}
+	if err := AssertThat(string(org), Is(content)); err != nil {
+		t.Fatal(err)
+	}
+	if err := AssertThat(string(b.Bytes()), Is(content)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestImplementsShadowLong(t *testing.T) {
+	content := createString(10000)
+	b := New(newReaderCloser(content))
+	org, err := ioutil.ReadAll(b)
+	if err := AssertThat(err, NilValue()); err != nil {
+		t.Fatal(err)
+	}
+	if err := AssertThat(string(org), Is(content)); err != nil {
+		t.Fatal(err)
+	}
+	if err := AssertThat(string(b.Bytes()), Is(content)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func createString(l int) string {
+	result := bytes.NewBufferString("")
+	for i := 0; i < l; i++ {
+		result.WriteString("a")
+	}
+	return result.String()
+}
+
+type nopCloser struct {
+	io.Reader
+}
+
+func (nopCloser) Close() error {
+	return nil
+}
+
+func NopCloser(r io.Reader) io.ReadCloser {
+	return nopCloser{r}
+}
