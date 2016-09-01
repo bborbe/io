c2c48bf5bd136037d775f6fabe5caaba93b341b5
Benjamin Borbe <bborbe@rocketnews.de>
2016-09-01 13:59:00 +0200

    improve logging

--- util/io_util.go
+++ util/io_util.go
@@ -38,10 +38,16 @@ func NormalizePath(path string) (string, error) {
 	if strings.Index(path, "~/") == 0 {
 		home := os.Getenv("HOME")
 		if len(home) == 0 {
+			glog.V(2).Infof("normalize path failed, enviroment variable HOME missing")
 			return "", fmt.Errorf("env HOME not found")
 		}
 		path = fmt.Sprintf("%s/%s", home, path[2:])
 		glog.V(2).Infof("replace ~/ with homedir. new path: %s", path)
 	}
-	return filepath.Abs(path)
+	result, err := filepath.Abs(path)
+	if err != nil {
+		glog.Warningf("get absolute path for %v failed: %v", path, err)
+		return "", err
+	}
+	return result, nil
 }
